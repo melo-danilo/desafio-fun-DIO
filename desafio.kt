da4d2204647a28b90819ed1d7f8d86382ae43ecc
Danilo Melo <54605570+melo-danilo@users.noreply.github.com>
2024-02-21 20:21:52 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,28 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val name: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    infix fun matricular(usuario: Usuario) {
+       inscritos.add(usuario)
     }
+   
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val danilo = Usuario("Danilo")
+    val funcoes = ConteudoEducacional("O poder das funcoes em kotlin", 40, Nivel.INTERMEDIARIO)
+    val android = Formacao("Formação Android", listOf(funcoes))
+    
+    android matricular danilo
+    
+    print(android.inscritos)
+    
+   
 }
